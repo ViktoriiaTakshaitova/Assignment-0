1c010c059e9c6013cffa8c8ffd376ba978861d6f
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 02:52:53 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,20 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  nums = new Array (2,4,5,6,23);
+  var count=0;
+  start=2;
+  end=23;
+  
+  for (var i=0; i< nums.length; i++)
+    {
+      if (nums[i]>=start && nums[i]<=end)
+        {
+         count++;
+        }
+    }
+ console.log(count);
 }
 
 // Do not edit this line;
-module.exports = sumOfNumsWithinARange;
+//module.exports = sumOfNumsWithinARange;
+sumOfNumsWithinARange();
