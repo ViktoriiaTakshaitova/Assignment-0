9c72eed74e64fcd92703a7ffb5d3bebb9ccd9174
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 02:38:02 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,18 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  nums = new Array (0,1,2,3,4,5);
+  var sum=0;
+  
+  for (var i=0; i< nums.length; i++)
+    {
+      if (nums[i]>0 && nums[i]%2==0)
+        {
+         sum+=nums[i];
+        }
+    }
+  console.log(sum);
 }
 
 // Do not edit this line;
-module.exports = sumOfAllEvenNumbers;
+//module.exports = sumOfAllEvenNumbers;
+sumOfAllEvenNumbers();
