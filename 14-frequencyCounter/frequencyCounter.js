73fd0428f4c0479f716b0811f2edf705754b20bd
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 15:55:36 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,21 @@
 function frequencyCounter(word) {
   // Insert code here;
+word=word.toLowerCase();//.split('');
+var count=1;
+for (var i=0; i < word.length; i++)
+  for(var j=i+1; j < word.length; j++)
+  {
+   if (word[i] == word[j])
+      {
+       count++;
+       } 
+      else
+        {
+          count=1;
+        }
+   document.write( word[i] + ":"+ count + ",\t");
+  }
 }
-
 // Do not edit this line;
-module.exports = frequencyCounter;
+//module.exports = frequencyCounter;
+frequencyCounter("DooD");
