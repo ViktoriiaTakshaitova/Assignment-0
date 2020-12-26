1590cb44ea7a2d2e8d7869e9c477851601221c19
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 00:54:34 -0500

    Update subtractFive.js

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,6 +1,13 @@
 function subtractFive(num) {
   // Insert code here;
+  num = new Array (20,30,40,50);
+  for(i=0;i<num.length;i++)
+    {
+     num[i]-=5;
+    }
+  console.log(num);
 }
 
 // Do not edit this line;
-module.exports = subtractFive;
+//module.exports = subtractFive;
+subtractFive();
