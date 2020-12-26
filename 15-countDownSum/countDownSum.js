8c7898149bd454206fa75352092e69b31c6a5d72
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 16:21:44 -0500

    Update countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,17 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    var nnum=0;
+  for (var i=0; i<=num; i++)
+    {
+       nnum+=i;
+    }
+    return(nnum);
   }
 }
 
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
+var studentSolution= new MySolution();
+console.log(studentSolution.countDownSum(10));//55
