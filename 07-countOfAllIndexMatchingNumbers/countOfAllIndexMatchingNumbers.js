b104fe6bd5e6618e6b0e32991e53c8baef800ae3
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 02:24:44 -0500

    Update countOfAllIndexMatchingNumbers.js

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,18 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  nums = new Array (0,1,2,3,4,5);
+  var count=0;
+  
+  for (var i=0; i< nums.length; i++)
+    {
+      if (nums[i]==i)
+        {
+          count++;
+        }
+    }
+  console.log(count);
 }
 
 // Do not edit this line;
-module.exports = countOfAllIndexMatchingNumbers;
+//module.exports =countOfAllIndexMatchingNumbers;
+countOfAllIndexMatchingNumbers();
