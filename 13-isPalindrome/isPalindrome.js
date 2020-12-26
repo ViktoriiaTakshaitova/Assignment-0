a7e638d954c7a58989fda5c4b543a2279ef046ad
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 14:36:23 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Insert code here;
+  word=word.toLowerCase();
+  var nword=word.split('').reverse().join('');
+  if (word == nword)
+    {
+      return true;
+    }
+ else
+   {
+     return false;
+   }
 }
-
 // Do not edit this line;
-module.exports = isPalindrome;
+//module.exports = isPalindrome;
+console.log(isPalindrome("Doodood"));
