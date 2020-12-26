6dda20c19720417347233906d3be9b4241a79199
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 13:40:23 -0500

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,29 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  
+  for (var i=0; i<end+1; i++)
+   
+    {
+      if (i/3==0)
+        {
+          document.write(" "+ "Fizz" + " ");
+        }
+       if (i/5==0)
+        {
+          document.write(" "+"Buzz" + " ");
+        }
+       if (i/3==0 && i/5==0)
+        {
+          document.write(" " + "FizzBuzz" + " ");
+        }
+       else // (i/3!=0 && i/5!=0)
+        {
+          document.write(i); //console.log
+        }
+    }
+ 
 }
 
 // Do not edit this line;
-module.exports = fizzBuzz;
+//module.exports = fizzBuzz;
+fizzBuzz(1,5);
