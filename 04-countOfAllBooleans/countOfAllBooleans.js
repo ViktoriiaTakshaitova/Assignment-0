eed4262a6404a89c9aa77df1bb08e7b2bd2ab58e
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 01:39:19 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,16 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  arr = new Array ("hello", "world", 3, "nice", 99, Infinity, -Infinity, true, true, true, false);
+  var count=0;
+  for(var i=0; i<arr.length; i++){
+  if (arr[i]==true || arr[i]==false)
+  {
+    count++;
+  }
+  }
+console.log(count);
 }
 
 // Do not edit this line;
-module.exports = countOfAllBooleans;
+//module.exports = countOfAllBooleans;
+  countOfAllBooleans();
