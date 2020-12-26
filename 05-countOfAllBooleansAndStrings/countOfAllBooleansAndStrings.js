76c1b2ba8644109855b6a4ce4aff48d6e1f7d5e2
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 02:05:27 -0500

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,16 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  arr = new Array ("hey", "there", "hello", "there", "world", false);
+  var count=0;
+  for(var i=0; i<arr.length; i++) {
+  if (arr[i]!="" || arr[i]==true || arr[i]==false)
+  {
+    count++;
+  }
+  }
+console.log(count);
 }
 
 // Do not edit this line;
-module.exports = countOfAllBooleansAndStrings;
+//module.exports = countOfAllBooleansAndStrings;
+  countOfAllBooleansAndStrings();
