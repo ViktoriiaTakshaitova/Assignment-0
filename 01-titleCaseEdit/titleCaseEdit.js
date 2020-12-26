1c0d6d94c5db1d8292618304f918686629299467
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 00:27:19 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,18 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  function titleCase(title) {
+    title=title.toLowerCase();
+    title=title.split(' ');
+  for (var i=0; i<title.length; i++)
+    {
+      title[i]=title[i].charAt(0).toUpperCase()+title[i].slice(1);
+    }
+      console.log(title.join(' '));
+      return title;
+}
+
 }
 
 // Do not edit this line;
-module.exports = titleCaseEdit;
+//module.exports = titleCaseEdit;
+ titleCase("the cow jumped over the moon");
