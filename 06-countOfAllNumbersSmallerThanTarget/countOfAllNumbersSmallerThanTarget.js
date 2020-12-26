085417e4799b8bca81aa522a21e8c9bca17839c4
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-26 02:15:44 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,18 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  num = new Array  (1,2,3,4,5);
+  target=9;
+  var count=0;
+  for (var i=0; i <num.length; i++)
+    {
+      if (num[i] < target)
+        {
+          count++;
+        }
+    }
+   console.log(count);
 }
 
 // Do not edit this line;
-module.exports = countOfAllNumbersSmallerThanTarget;
+//module.exports = countOfAllNumbersSmallerThanTarget;
+countOfAllNumbersSmallerThanTarget();
