069b49171914f848c00ee631b1de4c543e7a58cb
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-27 18:18:28 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,25 @@
 function pairSum(nums, target) {
   // Insert code here;
+
+    nums = [1,2];//new Array();
+    target =3 ;
+   
+  for (var i=0; i<nums.length; i++)
+    {
+      for (var j=i; j<nums.length; j++)
+    {
+      if (nums[i]+nums[j]==target)
+        {
+          return true;
+        }
+      else
+        {
+          return false;
+        }
+    }
+}
 }
 
 // Do not edit this line;
-module.exports = pairSum;
+//module.exports = pairSum;
+document.write(pairSum());//console.log
