b3cfe8a105463c0bb210d701d2cb25dcec0dbf60
ViktoriiaTakshaitova <vtoritak@gmail.com>
2020-12-27 22:12:21 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,24 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  // Insert code here;  
+ var num=[1,2,3];
+  var nnum=[4,5,6];
+
+for (var i=0; i < num.length; i++){
+  
+  for(var j=i; j < nnum.length; j++)
+  {
+   if (num[i] == nnum[j])
+      {
+        console.log(num[i]);
+       } 
+      else
+        {
+          console.log(num[i] + num[j]) ;                   
+        }
+  }
 }
+  }
 
 // Do not edit this line;
-module.exports = setUnionOfAnyAmountOfSets;
+//module.exports = frequencyCounter;
+setUnionOfAnyAmountOfSets();
