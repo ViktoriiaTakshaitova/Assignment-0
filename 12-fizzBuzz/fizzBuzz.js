3363b0e8479854afba833435731b50fb043c4949
ViktoriiaTakshaitova <vtoritak@gmail.com>
2021-01-03 11:46:09 -0500

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -3,18 +3,18 @@ function fizzBuzz(start, end) {
   // Insert code here;
 for( var i = 1; i <=end; i++)
 {
-    if (i % 5 == 0 && i % 3 == 0) 
-    {
-        console.log(' FizzBuzz ')
-    }
-  else if (i % 5 == 0) 
+    if (i % 5 == 0) 
   {
          console.log(' Buzz ');
-  }
+  } 
   else if (i % 3 == 0) 
   {
        console.log(' Fizz ');
   }
+  else if (i % 5 == 0 && i % 3 == 0) 
+    {
+        console.log(' FizzBuzz ')
+    }
   else
   {
       console.log(' '+ i);
@@ -24,3 +24,4 @@ for( var i = 1; i <=end; i++)
 // Do not edit this line;
 //module.exports = fizzBuzz;
 fizzBuzz(1,15);
+ 
