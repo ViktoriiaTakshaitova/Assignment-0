07ef99bca4fda4db369b5f994ca8448c9449ee70
ViktoriiaTakshaitova <vtoritak@gmail.com>
2022-02-16 21:24:05 -0500

    Update countOfAllIndexMatchingNumbers.js

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,16 +1,16 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
   nums = new Array (0,1,2,3,4,5);
-  var count=0;
+  let res=0;
   
   for (var i=0; i< nums.length; i++)
     {
       if (nums[i]==i)
         {
-          count++;
+          res+=nums[i];
         }
     }
-  console.log(count);
+  console.log(res);
 }
 
 // Do not edit this line;
